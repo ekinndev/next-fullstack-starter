933b455612abbbf5050c8b902b2bf9fdd703b23c
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-10-01 23:53:38 +0300

    Fix hot reload

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,15 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable arrow-parens */
 const { i18n } = require('./next-i18next.config');
 
 module.exports = {
   i18n,
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+
+    return config;
+  },
 };
